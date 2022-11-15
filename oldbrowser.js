cef7170ea9df06963326b08ebb583b252a71425f
stysan <55314369+stysan@users.noreply.github.com>
2022-11-15 22:39:06 +0300

    Create oldbrowser.js

---
+++ oldbrowser.js
@@ -0,0 +1,4 @@
+if (window.document.documentMode) or (typeof 'es8test'.padStart == "undefined") {
+  alert('Your browser is no longer supported. You will be redirected to firefox.com now.');
+  document.location = "http://firefox.com/";
+}
