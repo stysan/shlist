56c8e5d10cb6d98aac409f58d7ee6f4d1f5b782e
stysan <55314369+stysan@users.noreply.github.com>
2022-11-18 21:28:15 +0300

    Imagine commiting buggy code to prod 💀

--- oldbrowser.js
+++ oldbrowser.js
@@ -1,4 +1,7 @@
-if (window.document.documentMode) or (typeof 'es8test'.padStart == "undefined") {
-  alert('Your browser is no longer supported. You will be redirected to firefox.com now.');
+if (window.document.documentMode) {
+  alert('Internet Explorer is no longer supported. You will be redirected to firefox.com now.');
   document.location = "http://firefox.com/";
-}
+} else if (typeof 'es8test'.padStart == "undefined") {
+  alert('Your browser is obsolete (meaning your browser is from 2016 or older). You will be redirected to firefox.com now.');
+  document.location = "http://firefox.com/";
+} 
